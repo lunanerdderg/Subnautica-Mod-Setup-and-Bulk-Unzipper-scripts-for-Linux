080eb7a852cd0bf4cfb2cdf7db8a702a607b4986
Luna the Nerd Derg <nerddergontoastspam@gmail.com>
2026-01-05 06:58:24 +0000

    First upload!

---
+++ install.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+
+changedDirectory="n"
+subnauticaDirectory="$HOME/.local/share/Steam/steamapps/common/Subnautica/"
+ARG1=${1:-"--None"}
+ARG2=${2:-"--None"}
+ARG3=${3:-"--None"}
+ARG4=${4:-"--None"}
+ARG5=${5:-"--None"}
+
+if [ $ARG1 = "-h" ] || [ $ARG1 = "--help" ] || [ $ARG2 = "-h" ] || [ $ARG2 = "--help" ] || [ $ARG3 = "-h" ] || [ $ARG3 = "--help" ] || [ $ARG4 = "-h" ] || [ $ARG4 = "--help" ] || [ $ARG5 = "-h" ] || [ $ARG5 = "--help" ]; then
+    echo "Every listed parameter is optional, and may be used in any order."
+    echo
+    echo "    -h, --help               Display this guide"
+    echo "    path/to/directory        Use path as Subnautica game directory"
+    echo "    -n, --no-tweaks          Do not install Toebeann's BepInEx Tweaks for Subnautica"
+    echo "    -t, --terrain-patcher    Install Esper89's Terrain Patcher mod"
+    echo "    -d, --disable-autoopen   Do not open Subnautica automatically. Allow user to do it manually instead"
+    echo "    -i, --ignore-prompt      Disable most user prompts (do not use if first time running)"
+    exit 0
+fi
+
+if [ $ARG1 != "-"* ]; then
+    subnauticaDirectory=$ARG1
+    changedDirectory="y"
+elif [ $ARG2 != "-"* ]; then
+    subnauticaDirectory=$ARG2
+    changedDirectory="y"
+elif [ $ARG3 != "-"* ]; then
+    subnauticaDirectory=$ARG3
+    changedDirectory="y"
+elif [ $ARG4 != "-"* ]; then
+    subnauticaDirectory=$ARG4
+    changedDirectory="y"
+elif [ $ARG5 != "-"* ]; then
+    subnauticaDirectory=$ARG5
+    changedDirectory="y"
+fi
+if [ $ARG1 = "-n" ] || [ $ARG1 = "--no-tweaks" ] || [ $ARG2 = "-n" ] || [ $ARG2 = "--no-tweaks" ] || [ $ARG3 = "-n" ] || [ $ARG3 = "--no-tweaks" ] || [ $ARG4 = "-n" ] || [ $ARG4 = "--no-tweaks" ] || [ $ARG5 = "-n" ] || [ $ARG5 = "--no-tweaks" ]; then
+    tweaks="n"
+else
+    tweaks="y"
+fi
+if [ $ARG1 = "-t" ] || [ $ARG1 = "--terrain-patcher" ] || [ $ARG2 = "-t" ] || [ $ARG2 = "--terrain-patcher" ] || [ $ARG3 = "-t" ] || [ $ARG3 = "--terrain-patcher" ] || [ $ARG4 = "-t" ] || [ $ARG4 = "--terrain-patcher" ] || [ $ARG5 = "-t" ] || [ $ARG5 = "--terrain-patcher" ]; then
+    terrainPatcher="y"
+else
+    terrainPatcher="n"
+fi
+if [ $ARG1 = "-i" ] || [ $ARG1 = "--ignore-prompt" ] || [ $ARG2 = "-i" ] || [ $ARG2 = "--ignore-prompt" ] || [ $ARG3 = "-i" ] || [ $ARG3 = "--ignore-prompt" ] || [ $ARG4 = "-i" ] || [ $ARG4 = "--ignore-prompt" ] || [ $ARG5 = "-i" ] || [ $ARG5 = "--ignore-prompt" ]; then
+    ignorePrompt="y"
+else
+    ignorePrompt="n"
+fi
+if [ $ARG1 = "-d" ] || [ $ARG1 = "--disable-autoopen" ] || [ $ARG2 = "-d" ] || [ $ARG2 = "--disable-autoopen" ] || [ $ARG3 = "-d" ] || [ $ARG3 = "--disable-autoopen" ] || [ $ARG4 = "-d" ] || [ $ARG4 = "--disable-autoopen" ] || [ $ARG5 = "-d" ] || [ $ARG5 = "--disable-autoopen" ]; then
+    disableAutoopen="y"
+else
+    disableAutoopen="n"
+fi
+
+if ! type "grep" > /dev/null; then
+    echo "ERROR: grep is not installed."
+    exit 1
+fi
+if ! type "curl" > /dev/null; then
+    echo "ERROR: curl is not installed."
+    exit 1
+fi
+if ! type "wget" > /dev/null; then
+    echo "ERROR: wget is not installed."
+    exit 1
+fi
+if ! type "unzip" > /dev/null; then
+    echo "ERROR: unzip is not installed."
+    exit 1
+fi
+if ! type "7z" > /dev/null; then
+    echo "7zip is not installed, but is only necessary if you have a .7z mod."
+fi
+if ! type "unrar" > /dev/null; then
+    echo "unrar is not installed, but is only necessary if you have a .rar mod."
+fi
+
+if [ ! -d "$subnauticaDirectory" ]; then
+    subnauticaDirectory="$HOME/.local/share/Steam/steamapps/common/Subnautica/"
+    if [ ! -d "$subnauticaDirectory" ]; then
+        echo "Could not find Subnautica directory. Check if installed with Steam, or pass location as argument."
+        echo "(Usually '$HOME/.local/share/Steam/steamapps/common/Subnautica/')"
+        exit 1
+    fi
+fi
+
+echo "Installing BepInEx..."
+cd $subnauticaDirectory
+wget https://github.com/toebeann/BepInEx.Subnautica/releases/latest/download/Tobey.s.BepInEx.Pack.for.Subnautica.zip
+unzip Tobey.s.BepInEx.Pack.for.Subnautica.zip
+rm -f Tobey.s.BepInEx.Pack.for.Subnautica.zip
+cd $HOME
+
+if [ $ignorePrompt = "n" ]; then
+    echo ""
+    echo "Make sure you've set your Subnautica launch options as:"
+    echo 'WINEDLLOVERRIDES="winhttp=n,b" %command%'
+    echo "You can find this setting by going to Subnautica in your Steam Library, clicking the gear icon, and then clicking 'Properties'."
+    read -p "(Press enter to confirm you've set your game launch options. Do NOT open Subnautica.)"
+    if [ $disableAutoopen = "n" ]; then
+        echo ""
+        echo ""
+        echo ""
+        echo "Press enter if you understand that Steam must be FULLY closed after Subnautica is automatically opened."
+        echo "Merely pressing the 'X' on the top right will NOT work!"
+        read -p "You will need to click the Steam logo on the top left, then 'Exit'."
+    fi
+fi
+if [ $disableAutoopen = "n" ]; then
+    echo "TEST"
+    steam steam://rungameid/264710
+elif [ $disableAutoopen = "y" ]; then
+    read -p "Please open Subnautica, then quit it from the menu. Then press ENTER."
+    if [ $ignorePrompt = "n" ]; then
+        read -p "Are you sure you opened Subnautica, then quit from menu? Press ENTER if so."
+    fi
+fi
+
+echo "Installing Nautilus..."
+temp_dir=$(mktemp -d)
+cd ${temp_dir}
+git clone https://github.com/SubnauticaModding/Nautilus
+cd Nautilus
+curTag="$(git describe --tags --abbrev=0)"
+cd /
+rm -rf $temp_dir
+suffix=${curTag/-pre/}
+cd $subnauticaDirectory/BepInEx/plugins/
+wget "https://github.com/SubnauticaModding/Nautilus/releases/download/${curTag}/Nautilus_SN.STABLE_${suffix}.zip"
+unzip Nautilus_SN.STABLE_${suffix}.zip
+mv plugins/Nautilus Nautilus
+rm -f Nautilus_SN.STABLE_${suffix}.zip
+rm -rf plugins
+
+if [ $tweaks = "y" ]; then
+    echo "Installing BepInEx Tweaks..."
+    wget https://github.com/toebeann/BepInExTweaks.Subnautica/releases/latest/download/Tobey.BepInExTweaks.Subnautica.zip
+    unzip Tobey.BepInExTweaks.Subnautica.zip
+    mv BepInEx/plugins/Tobey/BepInEx\ Tweaks BepInEx\ Tweaks || mv BepInEx/plugins/BepInEx\ Tweaks BepInEx\ Tweaks
+    rm -rf BepInEx
+    rm -f Tobey.BepInExTweaks.Subnautica.zip
+fi
+
+if [ $terrainPatcher = "y" ]; then
+    echo "Installing Terrain Patcher..."
+    cd $subnauticaDirectory/BepInEx/plugins/
+    curl -s https://api.github.com/repos/Esper89/Subnautica-TerrainPatcher/releases/latest \
+    | grep "browser_download_url.*errainPatcher.zip" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi -
+    unzip TerrainPatcher.zip
+    rm -f TerrainPatcher.zip
+fi
+
+echo ""
+echo ""
+echo ""
+echo "Done!"
+echo "Now download any mods you like from Nexus or Github, and place them in '$subnauticaDirectory/BepInEx/plugins/' (WITHOUT unzipping)."
+if [ $changedDirectory = "n" ]; then
+    echo "Then run 'unzip-mods-in-bulk.sh', and you're done!"
+else
+    echo "Then run 'unzip-mods-in-bulk.sh -n $subnauticaDirectory', and you're done!"
+fi
+echo ""
+echo ""
+echo ""
+echo "(You can also place your mods in their own folder if you want to keep copies of them. If you decide to do that, then run:)"
+if [ $changedDirectory = "n" ]; then
+    echo "unzip-mods-in-bulk.sh <folder/zips/are/stored/in>"
+else
+    echo "unzip-mods-in-bulk.sh <folder/zips/are/stored/in> $subnauticaDirectory'"
+fi
---
+++ unzip-mods-in-bulk.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+directory=${1:-"--None"}
+subnauticaDirectory=${2:-"$HOME/.local/share/Steam/steamapps/common/Subnautica"}
+helpOrNot=${3:-"--None"}
+
+if [ $directory = "-h" ] || [ $directory = "--help" ] || [ $subnauticaDirectory = "-h" ] || [ $subnauticaDirectory = "--help" ]; then
+    echo
+    echo "All listed parameters are optional, but must be used IN ORDER. If you want to use the second but not the first, use '-n' or '--None'"
+    echo
+    echo "Example: unzip-mods-in-bulk.sh -n directory/of/Subnautica"
+    echo
+    echo "    -h, --help: Show this help menu"
+    echo "    1. Folder containing mod zips (defaults to 'Subnautica/BepInEx/plugins' folder)"
+    echo "    2. Directory of Subnautica game folder"
+    echo
+    echo "Unlike 'install.sh', this script has no user prompts."
+    echo
+    exit 0
+fi
+
+if [ $directory = "-n" ] || [ $directory = "--None" ]; then
+    directory=$(mktemp -d)
+    if [ ! -d "$subnauticaDirectory" ]; then
+        echo "Could not find Subnautica directory. Pass game folder as second argument."
+        echo "(If you never passed a first argument, pass '--None', then directory.)"
+        echo "(eg 'unzip-mods-in-bulk.sh --None <path/to/Subnautica>')"
+        exit 1
+    fi
+    for file in $subnauticaDirectory/BepInEx/plugins/*
+    do
+        if [[ "${file}" = *".zip" ]] || [[ "${file}" = *".7z" ]] || [[ "${file}" = *".rar" ]] || [[ "${file}" = *".tar"* ]]; then
+            mv "${file}" "${directory}/$(basename ${file})"
+        fi
+    done
+fi
+
+temp_dir=$(mktemp -d)
+echo "$temp_dir"
+echo
+for file in $directory/*
+do
+    echo $file
+    if [[ $file == *.zip ]] || [[ $file == *.7z ]] || [[ $file == *.rar ]] || [[ $file == *.tar* ]]; then
+        echo "Safe"
+        safe="y"
+        cd $directory
+        if [[ $file == *.zip ]]; then
+            unzip "$file" -d "$temp_dir"
+        elif [[ $file == *.7z ]]; then
+            if ! command -v 7z >/dev/null 2>&1
+            then
+                echo
+                echo
+                echo
+                echo "ERROR: No ability to decompress 7z. Please install 7zip"
+                echo
+                echo
+                echo
+                safe="n"
+            else
+                7z x "$file" -o"$temp_dir"
+            fi
+            elif [[ $file == *.rar ]]; then
+            if ! command -v unrar >/dev/null 2>&1
+            then
+                echo
+                echo
+                echo
+                echo "ERROR: No ability to decompress rar. Please install unrar or unrar-free"
+                echo
+                echo
+                echo
+                safe="n"
+            else
+                unrar x "$file" "$temp_dir"
+            fi
+        elif [[ $file == *.tar* ]]; then
+            tar –xf "$file" -C "$temp_dir"
+        fi
+        if [ $safe = "y" ]; then
+            cd $temp_dir
+            if [ -d "BepInEx" ]; then
+                for folder in BepInEx/plugins/*
+                do
+                    cd /
+                    mv "${temp_dir}/${folder}" "${subnauticaDirectory}/${folder}"
+                done
+                rm -rf $temp_dir/BepInEx
+            elif [ -d "plugins" ]; then
+                for folder in plugins/*
+                do
+                    cd /
+                    mv "${temp_dir}/${folder}" "${subnauticaDirectory}/BepInEx/${folder}"
+                done
+                rm -rf $temp_dir/plugins
+            else
+                for folder in *
+                do
+                    mv "${folder}" "${subnauticaDirectory}/BepInEx/plugins/${folder}"
+                done
+            fi
+        fi
+    fi
+done
+rm -rf $temp_dir
+cd $HOME
+if [ -d $subnauticaDirectory/BepInEx/plugins/Tobey ]; then
+    for file in $subnauticaDirectory/BepInEx/plugins/Tobey/*
+    do
+        temp="${file/Tobey\//}"
+        mv ${file// /\\ } ${temp// /\\ }
+    done
+    rm -rf $subnauticaDirectory/BepInEx/plugins/Tobey
+fi
+
+echo "Done! All your mods should now be playable!"

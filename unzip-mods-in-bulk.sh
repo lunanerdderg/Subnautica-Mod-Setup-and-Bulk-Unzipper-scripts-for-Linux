8ac009bcf428ed45489cecf6d67c1a93ffbfd65e
Luna the Nerd Derg <nerddergontoastspam@gmail.com>
2026-09-20 20:16:34 +0000

    Update unzip-mods-in-bulk.sh

--- unzip-mods-in-bulk.sh
+++ unzip-mods-in-bulk.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-# Location of Subnautica saves on Lutris: $HOME/.lutris/epic-games-store/drive_c/users/luna/AppData/LocalLow/Unknown Worlds/Subnautica/Subnautica/SavedGames
+# This is free and unencumbered software released into the public domain.
+
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+# For more information, please refer to <https://unlicense.org/>
 
 directory=${1:-"--None"}
 subnauticaDirectory=${2:-"$HOME/.local/share/Steam/steamapps/common/Subnautica"}
